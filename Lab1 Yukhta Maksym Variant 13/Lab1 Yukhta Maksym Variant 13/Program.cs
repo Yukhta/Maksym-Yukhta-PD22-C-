7d8eba6f82a3fefb8841702b9cd3507a4e09137a
Yukhta <72075478+Yukhta@users.noreply.github.com>
2020-12-16 21:31:43 +0200

    Add files via upload
    Lab1

---
+++ Lab1 Yukhta Maksym Variant 13/Lab1 Yukhta Maksym Variant 13/Program.cs
@@ -0,0 +1,24 @@
+﻿//YukhtaMaksym_PD-22
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab1_Yukhta_Maksym_Variant13
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.WriteLine("Обчислення периметру заданого n-кутника, описаного навколо кола радиусом R");
+            Console.Write("Кiлькiсть сторiн:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Радiус кола:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double P = n * (2 * r * Math.Sqrt(3));
+            Console.WriteLine($"Периметер {n} кутника дорвiнюе: {P}");
+            Console.ReadKey();
+        }
+    }
+}
