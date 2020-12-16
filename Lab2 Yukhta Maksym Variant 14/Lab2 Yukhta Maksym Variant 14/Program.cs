379f65c253969a0759fc2f5e0541186224ad7b08
Yukhta <72075478+Yukhta@users.noreply.github.com>
2020-12-16 22:48:13 +0200

    Add files via upload
    Lab2

---
+++ Lab2 Yukhta Maksym Variant 14/Lab2 Yukhta Maksym Variant 14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//YukhtaMaksym_PD-22
+namespace Lab1_Yukhta_Maksym_Variant14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nn, nk;
+
+            Console.WriteLine("nn=");
+            nn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("nk=");
+            nk = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey(); 
+
+            if (nn >= 0 && nk >= nn)
+            {
+                n = nn / 2;
+                int k = nn;
+                int a = 1;
+                for (int i = k; i < nk; i++)
+                {
+
+                    a = a * ((-1) ^ (k) * k + 8) / (2 * (k) ^ (2) + k + 7);
+                }
+                Console.WriteLine($"a={a}");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
