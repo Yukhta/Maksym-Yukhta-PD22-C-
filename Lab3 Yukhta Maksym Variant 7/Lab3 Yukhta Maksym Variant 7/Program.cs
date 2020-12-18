4de6818d92bd828a71c81971d2c97155a68f96fa
Yukhta <72075478+Yukhta@users.noreply.github.com>
2020-12-18 16:04:02 +0200

    Add files via upload

---
+++ Lab3 Yukhta Maksym Variant 7/Lab3 Yukhta Maksym Variant 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Yukhta_Maksym_Variant_7
+{
+    class Program
+    {
+        /*   7.	Перевірити істинність вислову:
+          "Серед трьох даних цілих чисел є хоч би одна пара співпадаючих".  */
+        static void Main()
+        {
+            int[] arr = new int[3];
+            string temp = "Чи знайшлась пара цiлих чисел якi спiвпадають? - ";
+
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write("Введiть " + (j + 1) + " Цiле число: ");
+                arr[j] = int.Parse(Console.ReadLine());
+            }
+            if (arr[0] == arr[1] || arr[0] == arr[2] || arr[1] == arr[2])
+            {
+                Console.WriteLine(temp + true);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(temp + false);
+                Console.ReadKey();
+            }
+        }
+    }
+}
