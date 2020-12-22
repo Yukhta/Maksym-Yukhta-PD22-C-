dfd2faebab72d268de719e21ebaa503c2cc6ebd3
Yukhta <72075478+Yukhta@users.noreply.github.com>
2020-12-22 20:12:12 +0200

    Add files via upload

---
+++ Lab4 Yukhta Maksym/Lab4 Yukhta Maksym/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Yukhta_Maksym 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("Розмiр масиву =");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            int[] b = new int[n];
+            int[] c = new int[n];
+
+
+
+            Console.WriteLine("A:\n");
+            for (int k = 0; k < a.Length; ++k)
+            {
+                a[k] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("B:\n");
+            for (int k = 0; k < b.Length; ++k)
+            {
+                b[k] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int ka = 0, kb = 0;
+
+            for (int k = 0; k < a.Length; ++k)
+            {
+                if ((ka >= 5) || ((a[ka] > b[kb]) && (kb < 5)))
+                {
+                    c[k] = b[kb];
+                    ++kb;
+                    Console.WriteLine("B:\n");
+                    Console.WriteLine(b[k]);
+                }
+                else
+                {
+                    c[k] = a[ka];
+                    ++ka;
+                    Console.WriteLine("A:\n");
+                    Console.WriteLine(a[k]);
+                }
+            }
+            Console.WriteLine("C:\n");
+            for (int k = 0; k < c.Length; ++k) { Console.WriteLine(c[k]); }
+
+            Console.ReadKey();
+        }
+    }
+}
