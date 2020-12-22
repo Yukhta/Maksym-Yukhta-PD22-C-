0fce1c18eea07368864f7ea54487df06a0d8fb43
Yukhta <72075478+Yukhta@users.noreply.github.com>
2020-12-22 20:22:15 +0200

    Add files via upload

---
+++ Lab 5 Yukhta Maksym/Lab 5 Yukhta Maksym/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr5_Yukhta_Maksym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, i, j;
+            Console.WriteLine("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("m=");
+            m = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[m, n];
+            Console.WriteLine("m=");
+            Random random = new Random();
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    a[i, j] = random.Next(1, 20);
+                    Console.Write("{0,4}", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+            int max = 0, min = 0;
+            for (i = 0; i < m; i++)
+            {
+                max = a[i, 0];
+                min = int.MaxValue;
+                for (j = 0; j < n; j++)
+                {
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                    }
+                    if (a[j, i] < min)
+                    {
+                        min = a[j, i];
+                    }
+                }
+                Console.WriteLine("max:{0}", max);
+                Console.WriteLine("min:{0}", min);
+            }
+            Console.ReadKey();
+        }
+    }
+}
