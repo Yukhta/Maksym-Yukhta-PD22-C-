b3e2a4721ae967d3547e8b5836eeee4d1f567738
Yukhta <72075478+Yukhta@users.noreply.github.com>
2021-06-22 10:55:59 +0300

    Add files via upload

---
+++ Semestr 2 Lab 4 Yukhta Maksym WPF/test-wpf/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace test_wpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            foreach (UIElement el in mainRoot.Children) {
+                if(el is Button)
+                    (el as Button).Click += button_Click;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e) {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "C")
+                textBox.Text = "";
+            else if(str == "=") {
+                string value = new DataTable().Compute(textBox.Text, null).ToString();
+                textBox.Text = value;
+            }
+            else
+                textBox.Text += str;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
